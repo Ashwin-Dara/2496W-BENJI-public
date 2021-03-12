2a845f6d0490b463fac4a1d38d175a0613bcd014
Ashwin D <niwhsa.d@gmail.com>
2021-03-12 10:13:17 -0800

    Updating the auto shoot logic, and making it so that there is one button needing to be pressed. The button will be R1 and it will essentially queue the balls and perform the same action for the queue. Maybe make the R2 remove balls from the queue?

--- include/lib/Intake.h
+++ include/lib/Intake.h
@@ -5,13 +5,15 @@
 #include "Config.h"
 
 #define INTAKE_MAX_VOLTAGE 127.0f
+#define DETECT_BALL_CONSTANT 2300
 
 namespace Intake{
+    extern int ball_shoot_queue; 
     bool R1, R2; 
     void opcontrol();
-    void ASNI();
-    void ASI();
+    void index(int dir); 
+    void sis(); 
+    void ASM();
 }
-
 
 #endif
--- src/lib/Intake.cpp
+++ src/lib/Intake.cpp
@@ -2,29 +2,54 @@
 #include "../include/lib/Intake.h"
 
 namespace Intake{
+    ball_shoot_queue = 0; 
+
     void opcontrol(){
         if(controller.get_digital(DIGITAL_L1)){
             intake_move(INTAKE_MAX_VOLTAGE);
+            index(1);
         }
         else{
+            sis(); 
             intake_move(0);
+            
         }
 
         if(controller.get_digital(DIGITAL_L2)){
             intake_move(-INTAKE_MAX_VOLTAGE);
+            index(-1);
         }
         else{
+            sis(); 
             intake_move(0);
         }
-        
+
         R1 = controller.get_digital(DIGITAL_R1);
         R2 = controller.get_digital(DIGITAL_R2);
     }
 
-    void ASNI(){
+    void index(int dir){
+        uptake.move(120 * dir);
+        outake.move(-120 * dir);
+    }
+
+    void sis(){
+        uptake.move(0);
+        outake.move(0);
+    }
+
+    void ASM(){ //needs to be put into a seperate thread
+        
         if(controller.get_digital(DIGITAL_R1) && !R1){
-            
+            ball_shoot_queue++; 
         }
+
+        if(controller.get_digital(DIGITAL_R1) && !R1){ 
+            while(line_sensor.get_value() > DETECT_BALL_CONSTANT){ //balls needs to be indexed already
+
+            }    
+        }
+        
     }
 
 
