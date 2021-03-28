87fdb3097a280a22e0d6c2c5635a38e570a21eba
Niwhsa Arad <48425033+Ashwn-Dar@users.noreply.github.com>
2021-03-27 20:45:20 -0700

    Update Config.h
    Post-product commenting

--- include/lib/Config.h
+++ include/lib/Config.h
@@ -1,6 +1,7 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+//Including all of the libraries that would be used throughout the rest of the code
 #include "main.h"
 #include <cmath>
 #include <string>
@@ -8,22 +9,32 @@
 #include <numeric>
 #include <iostream>
 
-#define RIGHT_DRIVE 1
+/**
+* Defining the port numbers of the robot's subsystems. 
+* 
+* Motors for the chassis are indicated by by "DRIVE". The chassis is a four motor drive. 
+* The intake will be indicated by "INTAKE". There are 2 intake motors situated on the front of the motor. 
+* There are two rotation sensors for each side of the drive and have the keyword "ENC."
+* The remaining motors are for the indexer (uptake) and the fly-wheel shooter (outtake). There are is one line sensor for detecting the ball within the robot. 
+* Finally, there is an IMU sensor. 
+*/
+
+#define RIGHT_DRIVE 1 //Chassis motors
 #define RIGHT_DRIVE_2 2
 
-#define LEFT_DRIVE 3
+#define LEFT_DRIVE 3 //Chassis motors
 #define LEFT_DRIVE_2 4
 
-#define RIGHT_INTAKE 5
+#define RIGHT_INTAKE 5 //Intakes
 #define LEFT_INTAKE 6
 
-#define UPTAKE 7
+#define UPTAKE 7 //Uptake/Flywheel
 #define OUTAKE 8
 
-#define RIGHT_ENC 10
+#define RIGHT_ENC 10 //Rotation Sensors 
 #define LEFT_ENC 9
 
-#define LINE_SENSOR 12
+#define LINE_SENSOR 12 //Misc. 
 #define IMU_SENSOR 13
 
 using namespace pros; 
