68b94271d0863774f13c3537d5901fb7f82fbeae
Niwhsa Arad <48425033+Ashwn-Dar@users.noreply.github.com>
2021-04-01 17:34:21 -0700

    Update Intake.h
    Beginning the commenting

--- include/lib/Intake.h
+++ include/lib/Intake.h
@@ -1,18 +1,23 @@
 #ifndef INTAKE_H_
 #define INTAKE_H_
 
+//INCLUDING FILES
+//CONFIG INCLUDES MAIN
 #include "Config.h"
 #include "PID.h"
 
 #define INTAKE_MAX_VOLTAGE 127.0f
-#define DETECT_BALL_CONSTANT 2300
+#define DETECT_BALL_CONSTANT 2300 //value of the light sensor when a ball is detected
 
-extern pros::Task ASM_task; 
+extern pros::Task ASM_task; //Autoshoot task
 
 namespace Intake{
-    extern int ball_shoot_queue; 
+    extern int ball_shoot_queue; //balls requested to be shot
+    
+    //used for the rising edge to increase ball queue count
     extern bool start_task; 
     extern bool R1, R2; 
+    
     void opcontrol();
     void index(int dir); 
     void sis(); 
